16852411a1f0827eaca02cf29e09ae9a875937c8
Ilkin G. Aghayev <ilkinga@code.local>
2020-11-10 17:34:33 +0400

    Test

---
+++ 10112020/Test/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Account
+    {
+
+      private string _username { get; set; }
+       public string username { get => _username;  }
+
+       private string _passwword { get; set; }
+       public  string passwword { get => _passwword; }
+
+        private int _no { get; set; }
+        public int No { get => _no; }
+       
+
+      public Account(string username, string password)
+        {
+            
+        }
+
+    }
+}
